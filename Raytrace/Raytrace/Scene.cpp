ddea1097836d56ac103b9a5e68511b6328e52a14
Julius Kördel <kordel.julius@gmail.com>
2018-09-24 15:14:26 +0200

    till erik

--- Raytrace/Raytrace/Scene.cpp
+++ Raytrace/Raytrace/Scene.cpp
@@ -30,6 +30,7 @@ std::vector<Intersectionobjects<Triangle>> Scene::rayIntersectionforlist(Ray arg
 		if (didintersect == true) {
 			Intersector.object = tri;
 			Intersector.point = intersect - tri.getnormal().getDir();
+
 			intersections.push_back(Intersector);
 		}
 	};
