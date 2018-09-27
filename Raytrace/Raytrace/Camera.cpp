b171f6690cc5983c4943b4a2b6265dcf324645e2
erika <erik.azp@hotmail.se>
2018-09-27 15:42:13 +0200

    start på createImage och tillagd constructor på color

--- Raytrace/Raytrace/Camera.cpp
+++ Raytrace/Raytrace/Camera.cpp
@@ -19,4 +19,12 @@ void Camera::render()
 
 void Camera::createImage()
 {
+	for (int h = 0; h < HEIGHT; ++h)
+		for (int w = 0; w < WIDTH; ++w)
+		{
+			ColorDbl def(0.0);
+			Pixel myPixel(def);
+			
+			// Läsa subpixlar för att få fram den hela pixeln
+		}
 }
