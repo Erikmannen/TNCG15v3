ec8e2ea5ffabd9dad07dd6e5a023870371ba3034
Julius Kördel <kordel.julius@gmail.com>
2018-10-01 15:37:01 +0200

    färdig intersection

--- Raytrace/Raytrace/Sphere.cpp
+++ Raytrace/Raytrace/Sphere.cpp
@@ -1,4 +1,4 @@
-#include "Sphere.h"
+﻿#include "Sphere.h"
 #define MINVALUE 1e-5f
 
 
@@ -9,45 +9,47 @@ Sphere::~Sphere()
 bool Sphere::SphereIntersection(Ray arg, glm::vec3& intersect)
 {
 	//TODO
-	// setting up mark notation 
+	// setting up mark notation fö 6 
 	// I = normalized dirr
 	glm::vec3 I = glm::normalize( arg.getend().getcoords() - arg.getstart().getcoords());
 	Vertex o = arg.getstart();
 	Vertex c = position;
 	float r = radius;
-
 	glm::vec3 oc = o.getcoords() - c.getcoords();
-
-	float b = glm::dot(2.0f * I, oc);
+	// substitutions
 	float a = glm::dot(I, I);
+	float b = glm::dot(2.0f * I, oc);
+	float ac = glm::dot(oc , oc) - glm::pow(r, 2);
+	// ad^2+bd+ac= 0 <=> d=-b/2 +- √((b/2)^2 + ac)
+	//+,- => 2 solutions
 	float d1 = -b / 2.0f;
 	float d2 = d1;
+	float rest = glm::pow(d1, 2) - ac;
+	d1 += rest;
+	d2 -= rest;
 
+	if (rest < MINVALUE)
+		return false;
 
-	float delta = d1 * d1 - (glm::dot(oc, oc) - r * r);
-	if (delta < -MINVALUE) {
-		return false; // complex solutions
+	rest = glm::sqrt(rest);
+	if (d1 <= 0 && d2 <= 0) {
+		return false;
 	}
-	else if (delta > -MINVALUE && delta < MINVALUE) {
-		//intersect = d1;
+	else if (d2 < d1) {
+		intersect = o.getcoords() + d2 * I;
+		return true;
+	}
+	else if (d1 < d2) {
+		intersect = o.getcoords() + d1 * I;
+		return true;
+	}
+	else {
+		intersect = o.getcoords() + d2 * I;
 		return true;
 	}
 
-	float dSqrt = std::sqrt(delta);
-
-	d1 -= dSqrt;
-	d2 += dSqrt;
-
-	if (d1 > MINVALUE) {
-		//intersect = d1;
-		return true;
-	}
-
-	else if (d2 > MINVALUE) {
-		//intersect = d2;
-		return true;
-	}
+	//This will never happen.
 	return false;
-
-
 }
+
+
